636ebda8156f4e9c5bdd98606ca606b5cfe59d1b
sushantkr961 <sushantonly961@gmail.com>
2023-01-25 11:51:40 +0530

    ok

--- db/connection.js
+++ db/connection.js
@@ -1,20 +1,11 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-mongoose.set("strictQuery", false);
-
 let connect = () => {
-  mongoose
-    .connect(process.env.DB_URL, {
-      // userNewUrlParser: true,
-      // useCreateIndex: true,
-      // useUnifiedTopology: true,
-      // useFindAndModify: false,
-    })
-    .then(() => {
-      console.log(`connection successful`);
-    })
-    .catch((e) => console.log(`no connection`));
+  mongoose.set("strictQuery", false);
+  return mongoose.connect(
+    "mongodb+srv://mernyoutube:mernyoutube@cluster0.nj5g05u.mongodb.net/?retryWrites=true&w=majority"
+  );
 };
 
 module.exports = connect;
